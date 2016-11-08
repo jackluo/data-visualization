70103d40dab1dc2780c4e45afa13ce08233cbb69
Jack Luo <thejackluo@gmail.com>
2016-11-07 23:16:25 -0500

    Changes

--- upload.py
+++ upload.py
@@ -5,18 +5,19 @@ start_time = time.time()
 
 ##################### HEAD #######################
 
+import collections
 import os
 import csv
-import requests
 
-import plotly.plotly as py
-from plotly.graph_objs import *
-from plotly.grid_objs import Column, Grid
+import requests
+from requests.auth import HTTPBasicAuth
 
 #################### CONFIG ######################
 
 base_directory = r"Downloads"
-# ADD PRIVATE API KEY HERE TO CHANGE ACCOUNT
+#base_directory = r"Test"
+auth = HTTPBasicAuth('datasets', 'w581yrzokp')
+headers = {'Plotly-Client-Platform': 'python'}
 
 ################## FUNCTIONS #####################
 
@@ -25,15 +26,15 @@ base_directory = r"Downloads"
 def get_filenames(base_directory):
 
     list_of_filenames = []
-
+    list_of_directories =set()
     for root, directories, filenames in os.walk(base_directory):
-
+        list_of_directories.add(root)
         for filename in filenames:
 
             filename = os.path.join(root,filename)
             if not filename.endswith(".DS_Store") : list_of_filenames.append(filename)
 
-    return list_of_filenames
+    return list_of_filenames, sorted(list_of_directories)
 
 
 # column name is header of the csv. corresponding to the first row of the csv
@@ -54,17 +55,31 @@ def extract_column_data(filename, column_names):
     return columns
 
 
+# beautifies the filename for upload
 def get_title(filename):
 
     if filename.endswith(".csv"):
         filename = filename[:-4]
     
-    filename = filename.replace("Downloads/", "").replace("_", " ").replace("-", " ").replace(".", " ")
+    if filename.startswith(base_directory):
+        filename = filename[len(base_directory):]
+
+    filename = filename.replace("-", " ").replace(".", " ")
     return filename.title()
 
 
+# makes the folder payload to create a folder
+def make_folder(directory):
+
+    nested_dict = lambda: collections.defaultdict(nested_dict)
+    folder_payload = nested_dict()
+    
+    folder_payload["path"] = get_title(directory).replace("\\", '/')
+    return folder_payload
+
+
 # makes the Plotly Grid object with the columns and the title
-def make_grid(columns, column_names, filename):
+def make_payload(columns, column_names, filename, folder_payload):
 
     # check for duplicates in list as Plotly doesnt support columns with same name
     dups = {}
@@ -84,42 +99,69 @@ def make_grid(columns, column_names, filename):
             # Use stored occurrence value
             column_names[i] += str(dups[val][1])
 
+    # creates nested dictionnary
+    nested_dict = lambda: collections.defaultdict(nested_dict)
+    payload = nested_dict()
 
-    # list comprehension to create a list of Column objects used to make Grid
-    # get_title gets the capitalized title for csv file    
-    column_data = [Column(column, column_names[i]) for i, column in enumerate(columns)]
-    grid = Grid(column_data)
-    title = get_title(filename)
+    payload["filename"] = get_title(filename)
+    payload["title"] = get_title(filename)
+    payload["world_readable"] = True
+    payload["parent_path"] = folder_payload["path"]
+ 
+    for i, column in enumerate(columns):
+        key =column_names[i]
+        if key=='':
+            key='id'
+        payload["data"]["cols"][key]["order"] = i
+        payload["data"]["cols"][key]["data"] = column
 
-    return grid, title
+    return payload
 
 
+def create_folders(folders):
+  for folder in folders:
+    folder_payload = make_folder(folder)
+    print "Created folder" , folder_payload
+    folder_response = requests.post('https://api.plot.ly/v2/folders', auth=auth, headers=headers, json=folder_payload)
+
+    print "Response :", folder_response.text
+    
+
+def upload_files(filenames):
+    for filename in filenames:
+        try:
+            file = open(filename, 'rU')
+            print "Uplaod " , filename
+            # opens once to get the column names
+            column_names = csv.reader(file).next()
+            file.close()
+            column_names = [column_name.strip() for column_name in column_names]
+            #print filename, column_names
+            columns = extract_column_data(filename, column_names)    
+    
+            root, rootless_filename = os.path.split(filename)
+            folder_payload = make_folder(root)
+            payload = make_payload(columns, column_names, rootless_filename, folder_payload)
+            response = requests.post('https://api.plot.ly/v2/grids', auth=auth, headers=headers, json=payload)
+            print "GRID", response.text
+        except:
+           print "Fail upload " , filename
+  
+        
 ##################### MAIN #######################
 
 
-list_of_filenames = get_filenames(base_directory)
+filenames, folders = get_filenames(base_directory)
 
-for filename in list_of_filenames:
-
-    # open the csv first to get the column_names to use csv as dictreader
-    file = open(filename, 'rU')
-
-    column_names = csv.reader(file).next()
-    column_names = [column_name.strip() for column_name in column_names]
-
-    columns = extract_column_data(filename, column_names)    
-    
-    grid, title = make_grid(columns, column_names, filename)
-    print title
-
-    #grid_url = requests.get('https://api.plot.ly/v2/files/lookup', params={'user': 'datasets', 'path': title}).json()["web_url"]
-    #print grid_url
-    #py.grid_ops.delete(grid_url=grid_url)
-
-    grid_url = py.grid_ops.upload(grid, filename=title, world_readable=True, auto_open=True)
-
+create_folders(folders)
+upload_files(filenames)
 
 ##################### TIME #######################
-
 print "Done!"
 print "%s seconds" % (time.time() - start_time)
+
+
+ 
+
+
+ 
