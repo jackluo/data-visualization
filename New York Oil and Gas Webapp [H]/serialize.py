c2cd291e1eadea17c80f0d4692f02fdc632ee119
Jack Luo <thejackluo@gmail.com>
2017-06-01 17:39:29 -0400

    [ENH] Added Oil and Gas interactive webapp (made using Dash)

---
+++ New York Oil and Gas Webapp [H]/serialize.py
@@ -0,0 +1,35 @@
+# Import required libraries
+import pandas as pd
+import pickle
+
+# Load required dataframes
+df = pd.read_csv('data/wellspublic.csv', low_memory=False,
+                 index_col='API_WellNo')
+dataset = df.to_dict(orient='index')
+
+columns1 = ['API Well Number', 'Months in Production', 'Gas Produced, MCF',
+            'Water Produced, bbl', 'Reporting Year']
+columns2 = ['API Well Number', 'Months in Production', 'Gas Produced, MCF',
+            'Water Produced, bbl', 'Oil Produced, bbl', 'Reporting Year']
+df1 = pd.read_csv('data/Oil_and_Gas_Annual_Production__1985_-_2000.csv',
+                  low_memory=False)[columns1]
+df2 = pd.read_csv('data/Oil_and_Gas_Annual_Production__Beginning_2001.csv',
+                  low_memory=False)[columns2]
+
+# Concatenate dataframes
+df = pd.concat([df1, df2])
+
+# Append production data to each API
+columns = ['Months in Production', 'Gas Produced, MCF', 'Water Produced, bbl',
+           'Oil Produced, bbl', 'Reporting Year']
+for api, df_well in df.groupby('API Well Number'):
+    df_well = df_well[columns]
+    df_well.index = df_well['Reporting Year']
+    try:
+        dataset[api]['Production'] = df_well
+    except:
+        print('Failed to find', api)
+
+# Serialise to Pickle
+with open('data/dataset.pickle', 'wb') as f:
+    pickle.dump(dataset, f, protocol=pickle.HIGHEST_PROTOCOL)
